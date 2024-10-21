df6c685e6e6b6a60b902a74f19306c93a43f267d
Ayesha1130 <ayeshaiqbal771@gmail.com>
2024-10-21 21:43:33 +0500

    first commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,45 +1,71 @@
+"use client";
 import React from "react";
 import Link from "next/link";
+import { useState } from "react";
+import { IoMdMenu } from "react-icons/io";
+import { RxCross2 } from "react-icons/rx";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <div className=" flex flex-col md:flex-row item-center justify-between ma:mt-7 p-4 ">
-      <h1 className="font-bold text-2xl md:text-3xl italic text-center">
+    <header className="flex  bg-[#CF102D] justify-between h-20 p-4 items-center ">
+      <button title="button" onClick={toggleMenu}>
+        {!isOpen ? (
+          <IoMdMenu size={20} className="md:hidden lg:hidden block " />
+        ) : (
+          <RxCross2 size={20} className="md:hidden lg:hidden block " />
+        )}
+      </button>
+      <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold italic mr-20 ">
         Ayesha Iqbal
       </h1>
-      <ul className="flex justify-center gap-x-4  italic text-lg md:text-xl cursor-pointer ">
-        <Link
-          href={"/"}
-          className="hover:text-red-700 transition-transform hover:scale-110"
-        >
-          Home
-        </Link>
-        <Link
-          href={"/about"}
-          className="hover:text-red-700 transition-transform hover:scale-110"
-        >
-          About
-        </Link>
-        <Link
-          href={"/skills"}
-          className="hover:text-red-700 transition-transform hover:scale-110"
-        >
-          Skills
-        </Link>
-        <Link
-          href={"projects"}
-          className="hover:text-red-700 transition-transform hover:scale-110"
-        >
-          Projects
-        </Link>
-        <Link
-          href={"/contact"}
-          className="hover:text-red-700 transition-transform hover:scale-110 "
-        >
-          Contact
-        </Link>
-      </ul>
-    </div>
+      <nav className="hidden md:flex lg:flex flex-grow justify-center">
+        <ul className="text-2xl flex flex-row justify-center gap-14 font-semibold italic  ">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/skills">Skills</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+  
+          </li>
+          <li>
+            <Link href="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {isOpen && (
+        <nav className="md:hidden lg:hidden absolute top-20 left-0 w-full">
+          <ul className="flex flex-col font-semibold gap-4 p-4  bg-red-400 absolute  w-full  rounded-lg shadow-lg ">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/skills" >Skills</Link>
+            </li>
+            <li>
+              <Link href="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </header>
   );
 };
 
